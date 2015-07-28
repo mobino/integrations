0cd39ed71962621db4430865cd7e559c43fd4ed1
alvi <github@o08.net>
2015-07-28 16:18:31 +0200

    removed unused translations

--- gambio/lang/german/modules/payment/mobino.php
+++ gambio/lang/german/modules/payment/mobino.php
@@ -2,8 +2,6 @@
 define('MODULE_PAYMENT_MOBINO_TEXT_TITLE', 'Mobino payment');
 define('MODULE_PAYMENT_MOBINO_TEXT_DESCRIPTION', 'Mobino payment <br />Information: <a href="http://www.mobino.com" target="_blank">www.mobino.com</a>');
 define('MODULE_PAYMENT_MOBINO_TEXT_INFO', '');
-define('MODULE_PAYMENT_MOBINO_PSPID_TITLE', 'PSPID');
-define('MODULE_PAYMENT_MOBINO_PSPID_DESC', '');
 define('MODULE_PAYMENT_MOBINO_API_KEY_TITLE', 'Mobino API key');
 define('MODULE_PAYMENT_MOBINO_API_KEY_DESC', '');
 define('MODULE_PAYMENT_MOBINO_API_SECRET_TITLE', 'Mobino API secret');
@@ -16,8 +14,6 @@ define('MODULE_PAYMENT_MOBINO_SORT_ORDER_TITLE' , 'Anzeigereihenfolge');
 define('MODULE_PAYMENT_MOBINO_SORT_ORDER_DESC' , 'Reihenfolge der Anzeige. Kleinste Ziffer wird zuerst angezeigt');
 define('MODULE_PAYMENT_MOBINO_ORDER_STATUS_ID_TITLE' , 'Bestellstatus festlegen');
 define('MODULE_PAYMENT_MOBINO_ORDER_STATUS_ID_DESC' , 'Bestellungen, welche mit diesem Modul gemacht werden, auf diesen Status setzen');
-define('MODULE_PAYMENT_MOBINO_SHA1_SIGNATURE_TITLE', 'SHA-1 Signature');
-define('MODULE_PAYMENT_MOBINO_SHA1_SIGNATURE_DESC', 'Ihre SHA-1 Signature für die Datenüberprüfung');
 define('MODULE_PAYMENT_MOBINO_ALLOWED_TITLE' , 'Erlaubte Zonen');
 define('MODULE_PAYMENT_MOBINO_ALLOWED_DESC' , 'Geben Sie <b>einzeln</b> die Zonen an, welche f&uuml;r dieses Modul erlaubt sein sollen. (z.B. CH,AT,DE (wenn leer, werden alle Zonen erlaubt))');
 ?>
